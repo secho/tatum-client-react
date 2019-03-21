5df68d86935efad23f3286904d5b2fa984b23466
Sechovec Jan <cen39185@csin.cz>
2019-03-21 16:35:35 +0100

    :hammer: services added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import AnotherPage from './pages/AnotherPage';
 import { Context, initialState, reducer } from "./store";
+import { useDataApi } from './hooks/useDataApi';
 
 
 const App = () => {
 
   const [store, dispatch] = useReducer(reducer, initialState);
+  const { data, isLoading, isError, doFetch } = useDataApi(
+    'http://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] },
+  );
   
   return (
-    <Context.Provider value={{ store, dispatch }}>
+    <Context.Provider value={{ store, dispatch,
+                              service:  { data, isLoading, isError, doFetch}
+                               }}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ HomePage }/>
--- src/components/ClientsList/index.js
+++ src/components/ClientsList/index.js
@@ -1,16 +1,49 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, Fragment} from 'react';
 import { Context } from '../../store';
 
 
 const ClientsList = (props) => {
 
-    const {store} = useContext(Context);
+    const [query, setQuery] = useState('redux');
+    const {store, service} = useContext(Context);
     
     return (
         <div>
             {store.data.map((value, i) => 
                 <div key={i}>{value}</div>
             )}
+
+        <Fragment>
+                <form onSubmit={event => {
+                service.doFetch(
+                    `http://hn.algolia.com/api/v1/search?query=${query}`,
+                );
+                event.preventDefault();
+                }}>
+                <input
+                type="text"
+                value={query}
+                onChange={event => setQuery(event.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+
+            {service.isError && <div>Something went wrong ...</div>}
+
+            {service.isLoading ? (
+                <div>Loading...</div>
+            ): (
+                <ul>
+                {service.data.hits.map(item => (
+                    <li key={item.objectID}>
+                    <a href={item.url}>{item.title}</a>
+                    </li>
+                ))}
+                </ul>
+            )}
+
+            
+            </Fragment>
         </div>
     )
 }
